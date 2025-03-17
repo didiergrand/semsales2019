7e28e9f699815f9dc20df4cabef817dfac9ddcb5
Didier Grand <didier.grand@gmail.com>
2025-03-17 12:32:34 +0100

    Add child theme css

--- functions.php
+++ functions.php
@@ -7,6 +7,7 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'twentysixteen', get_template_directory_uri() . '/style.css', array(), '17.3.25' );
+	wp_enqueue_style( 'twentysixteen-child', get_stylesheet_directory_uri() . '/style.css', array(), '17.3.25' );
 
 }
 
