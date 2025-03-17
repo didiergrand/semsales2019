37807073e95f091fa305037d43184e23e5078cf8
Didier Grand <didier.grand@gmail.com>
2025-03-17 10:47:19 +0100

    Add version number for css

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
 
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
-    wp_enqueue_style( 'twentysixteen', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'twentysixteen', get_template_directory_uri() . '/style.css', array(), '17.3.25' );
 
 }
 
